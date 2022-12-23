9104494e5e9822eefe4c5afb902dd42dfc2fee06
maksumovas <118979305+maksumovas@users.noreply.github.com>
2022-12-23 23:45:56 +0600

    Update main.swift

--- 2lesson_2month/2lesson_2month/2hw_2month/2hw_2month/main.swift
+++ 2lesson_2month/2lesson_2month/2hw_2month/2hw_2month/main.swift
@@ -13,12 +13,16 @@ import Foundation
 //Свойства: Название товара, кол-во. Цена товара
 //После чего путём наследования от класса Магазин создать следующие классы: Магазин автозапчастей, Магазин одежды и заполнить их соответствующими параметрами
 
+//Так же в конце должен быть метод, который будет отображать следующую информацию:
+//
+//Название магазина. Площадь.
+//Расположение. Часы работы.
+//Наименование товара. Кол-во товара. Цена товара (Если у вас два и более одинаковых по названию товаров - соединить)
 class Store{
     var name: String
     var square: Int
     var location: String
     var workscedule: String
-    var products = ["Lipstick", "Mascara", "Bronzer"]
    
     init(name: String, square: Int, location: String, workscedule: String) {
         self.name = name
@@ -35,6 +39,7 @@ class Product{
     var name: String
     var quantity: Int
     var price: Int
+    var products = ["Lipstick", "Mascara", "Bronzer"]
     
     init(name: String, quantity: Int, price: Int) {
         self.name = name
@@ -68,5 +73,11 @@ autoParts.getInfo()
 var clothes = clothingStore(name: "MaxMara", square: 180, location: "Erkindik st", workscedule: "9:00-21:00")
 clothes.getInfo()
 
-var liptick = Product(name: "Charlotte Tilbury", quantity: 1, price: 2500)
+var liptick = Product(name: "Lipstick", quantity: 1, price: 2500)
 liptick.getInfo()
+
+var mascara = Product(name: "Mascara", quantity: 2, price: 1400)
+mascara.getInfo()
+
+var bronzer = Product(name: "Bronzer", quantity: 3, price: 2700)
+bronzer.getInfo()
